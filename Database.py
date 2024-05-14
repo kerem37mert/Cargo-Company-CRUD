88a3f6f02a6bc23eb4f6580156c6b34414f659af
Kerem Mert <kerem37mert@yahoo.com>
2024-05-14 13:32:20 +0300

    database.py updated

--- Database.py
+++ Database.py
@@ -2,8 +2,12 @@ import pyodbc
 
 class Database:
     def __init__(self):
-        self.cnxn = pyodbc.connect("Driver={SQL Server};"
-                              r"Server=DESKTOP-SKM8I25\SQLEXPRESS;"
-                              "Database=Cargo_Company;"
-                              "Trusted_Connection=yes;")
-        self.cursor = self.cnxn.cursor()
+        try:
+            self.cnxn = pyodbc.connect("Driver={SQL Server};"
+                                       r"Server=DESKTOP-SKM8I25\SQLEXPRESS;"
+                                       "Database=Cargo_Company;"
+                                       "Trusted_Connection=yes;")
+        except Exception as e:
+            print(f"Error: {e}")
+        else:
+            self.cursor = self.cnxn.cursor()
