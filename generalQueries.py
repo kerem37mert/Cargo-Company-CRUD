42e3ed3206e2dc6d0bf674abe8551d6a2ee5d335
Kerem Mert <kerem37mert@yahoo.com>
2024-05-14 13:21:19 +0300

    column max, column min and column sum functions added

--- generalQueries.py
+++ generalQueries.py
@@ -13,3 +13,23 @@ def columnAvg(columnName, tableName):
     db.cursor.execute(f"SELECT AVG({columnName}) FROM {tableName}")
     return db.cursor.fetchone()[0]
 
+
+#### Column Maximum ####
+def columnMax(columnName, tableName):
+    db = Database()
+    db.cursor.execute(f"SELECT MAX({columnName}) FROM {tableName}")
+    return db.cursor.fetchone()[0]
+
+
+#### Column Minimum ####
+def columnMax(columnName, tableName):
+    db = Database()
+    db.cursor.execute(f"SELECT MIN({columnName}) FROM {tableName}")
+    return db.cursor.fetchone()[0]
+
+
+#### Column Sum ####
+def columnSum(columnName, tableName):
+    db = Database()
+    db.cursor.execute(f"SELECT SUM({columnName}) FROM {tableName}")
+    return db.cursor.fetchone()[0]
