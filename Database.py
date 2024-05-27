a04b95715c61086511bf2d691e18c1f153886b89
Kerem Mert <kerem37mert@yahoo.com>
2024-05-27 13:35:45 +0300

    postgresql connection added

--- Database.py
+++ Database.py
@@ -4,11 +4,25 @@ class Database:
     def __init__(self):
         try:
 
-            # SQL Server
+            #SQL Server
             self.cnxn = pyodbc.connect("Driver={SQL Server};"
                                        r"Server=DESKTOP-SKM8I25\SQLEXPRESS;"
                                        "Database=Cargo_Company;"
                                        "Trusted_Connection=yes;")
+
+
+            #POSTGRESQL
+            # conn_str = (
+            #     "Driver={PostgreSQL Unicode};"
+            #     "Server=localhost;"
+            #     "Port=5432;"
+            #     "Database=cargo_company;"
+            #     "Uid=postgres;"
+            #     "Pwd=my_password;"         # Password input
+            # )
+            # self.cnxn = pyodbc.connect(conn_str)
+
+            print("connected to database")
 
         except Exception as e:
             print(f"Error: {e}")
